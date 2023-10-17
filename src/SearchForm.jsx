63ca68e39da3514aab79fd2bb4c6257ebef2a5f7
Lawrence Caulker <locaulker@gmail.com>
2023-10-17 06:12:08 -0400

    created search form with e.target.elements without useState()

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,4 +1,26 @@
 const SearchForm = () => {
-  return <h2>SearchForm</h2>
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const searchValue = e.target.elements.search.value
+    if (!searchValue) return
+    console.log(searchValue)
+  }
+
+  return (
+    <section>
+      <h1 className="title">Unsplash Images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="form-input search-input"
+          name="search"
+          placeholder="cat"
+        />
+        <button type="submit" className="btn">
+          Search
+        </button>
+      </form>
+    </section>
+  )
 }
 export default SearchForm
